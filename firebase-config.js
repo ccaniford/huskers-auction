91e391589a03a8b2c09a49b380cd3ff0fa3c79c0
ccaniford <ccaniford2@huskers.com>
2026-06-22 17:24:24 -0500

    Update Firebase config and auction open time
    Updated Firebase configuration with real credentials and changed auction open time.

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,14 @@
 const FIREBASE_CONFIG = {
-  apiKey:            "paste your apiKey here",
-  authDomain:        "paste your authDomain here",
-  databaseURL:       "paste your databaseURL here",
-  projectId:         "paste your projectId here",
-  storageBucket:     "paste your storageBucket here",
-  messagingSenderId: "paste your messagingSenderId here",
-  appId:             "paste your appId here"
+  apiKey:            "AIzaSyBgWf29g_2C1JcxuyKZ55qo0eImLiY7_9M",
+  authDomain:        "huskers-auction.firebaseapp.com",
+  databaseURL: "https://huskers-auction-default-rtdb.firebaseio.com",
+  projectId: "huskers-auction",
+  storageBucket: "huskers-auction.firebasestorage.app",
+  messagingSenderId: "496839822385",
+  appId: "1:496839822385:web:07dcdb3606d3998da69910"
 };
 
 const AUCTION_DEFAULTS = {
-  openTime:  '2026-06-24T10:00:00.000Z',
+  openTime:  '2026-06-22T10:00:00.000Z',
   closeTime: '2026-06-24T20:00:00.000Z'
 };
